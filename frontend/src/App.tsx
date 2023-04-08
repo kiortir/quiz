26145301859fdbcecbf7506864b5106ef2c67ce9
kiortir <mpgg2000@gmail.com>
2023-04-08 09:54:56 +0300

    options

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,17 @@
 import { useState } from 'react'
 import { Option } from './components/Option'
-import { MantineProvider, Button, Title, Center, Box, Container, Textarea, Select, Group, TextInput } from '@mantine/core'
+import {
+  MantineProvider,
+  Button,
+  Title,
+  Center,
+  Box,
+  Container,
+  Textarea,
+  Select,
+  Group,
+  TextInput
+} from '@mantine/core'
 
 /*
 {
@@ -16,12 +27,25 @@ import { MantineProvider, Button, Title, Center, Box, Container, Textarea, Selec
 }
 */
 
-function App() {
+function App () {
   const [question, setQuestion] = useState<string | undefined>(undefined)
-  const [answerType, setAnswerType] = useState<string | null>(null)
+  const [answerType, setAnswerType] = useState<string | null>('options')
+
+  const [list, setList] = useState<string[]>([])
+
+  const addOption = () => {
+    setList(list.concat(''))
+  }
+
+  const setOption = (value: string, index: number) => {
+    const l: string[] = Object.assign([], list)
+    l[index] = value
+    setList(l)
+  }
 
   return (
     <Container mt='xl'>
+      <span>{JSON.stringify(list)}</span>
       <Box mb='xl'>
         <Title order={2}>Добавление нового задания</Title>
       </Box>
@@ -30,7 +54,7 @@ function App() {
           <Textarea
             label='Введите вопрос задания'
             value={question}
-            onChange={(event) => setQuestion(event.currentTarget.value)}
+            onChange={event => setQuestion(event.currentTarget.value)}
             required
           />
         </Box>
@@ -40,7 +64,7 @@ function App() {
             placeholder='Выберите тип ответа на задачу'
             data={[
               { value: 'text', label: 'Тектовый ответ' },
-              { value: 'options', label: 'Ответ на викторину' },
+              { value: 'options', label: 'Ответ на викторину' }
             ]}
             value={answerType}
             onChange={setAnswerType}
@@ -48,24 +72,27 @@ function App() {
           />
         </Box>
         <Box mt='md'>
-          {
-            answerType ?
-              answerType === 'text' ?
-                <TextInput
-                  label='Правильный ответ'
-                  required
-                />
-              :
-                <Group>
-                  {/* <Button>
-                    Добавить ответ
-                  </Button> */}
-                  <Option
-
+          {answerType ? (
+            answerType === 'text' ? (
+              <TextInput label='Правильный ответ' required />
+            ) : (
+              <Group>
+                <Button onClick={addOption}>Добавить ответ</Button>
+                {list.map((item, index) => (
+                  <input
+                    key={index}
+                    type='text'
+                    value={item}
+                    onInput={e => setOption(e.currentTarget.value, index)}
                   />
-                </Group>
-            : ''
-          }
+                ))}
+
+                {/* <Option /> */}
+              </Group>
+            )
+          ) : (
+            ''
+          )}
         </Box>
       </Box>
     </Container>
